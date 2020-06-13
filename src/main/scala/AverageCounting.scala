af9c35f160a3661c46b543cda8d82de8c78e45db
Jakub Stawik <jakub.stawik@gmail.com>
2020-06-13 16:54:16 +0200

    AverageCounting with subNodes

--- src/main/scala/AverageCounting.scala
+++ src/main/scala/AverageCounting.scala
@@ -1,7 +1,12 @@
-import akka.actor.ActorRef
+import akka.actor._
 
 class AverageCounting(network: ActorRef, var data: Double) extends Node(network){
-  def receive: Receive = {
+  val maxNode: ActorRef =  context.actorOf(Props(classOf[PropagateMax], self), "maxModule")
+  val minNode: ActorRef =  context.actorOf(Props(classOf[PropagateMin], self), "minModule")
+  def averageCountingReceive : Receive = {
+    case message @ GiveValue(_, PropagateMaxType) => maxNode ! message
+    case message @ GiveValue(_, PropagateMinType) => minNode ! message
     case _ => logger.error(s"Unhandled message from ${sender().path.name}")
   }
+  override def receive: Receive = averageCountingReceive orElse commonReceive
 }
