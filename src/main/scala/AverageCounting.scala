de93caa687e8bf223970edb3d4f23f2eb91133cc
Jakub Stawik <jakub.stawik@gmail.com>
2020-06-13 17:36:28 +0200

    Fix AC not properly creating a subnet

--- src/main/scala/AverageCounting.scala
+++ src/main/scala/AverageCounting.scala
@@ -4,6 +4,9 @@ class AverageCounting(network: ActorRef, var data: Double) extends Node(network)
   val maxNode: ActorRef =  context.actorOf(Props(classOf[PropagateMax], self), "maxModule")
   val minNode: ActorRef =  context.actorOf(Props(classOf[PropagateMin], self), "minModule")
   def averageCountingReceive : Receive = {
+    case GiveNeighbour(neighbour) => neighs = neighs :+ neighbour
+      maxNode ! GiveNeighbour(neighbour)
+      minNode ! GiveNeighbour(neighbour)
     case message @ GiveValue(_, PropagateMaxType) => maxNode ! message
     case message @ GiveValue(_, PropagateMinType) => minNode ! message
     case _ => logger.error(s"Unhandled message from ${sender().path.name}")
