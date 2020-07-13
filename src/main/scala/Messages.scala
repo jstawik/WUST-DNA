7c5da8f18b8f19ad4fb6bb162a0a072ce546d207
jstawik <jakub.stawik@gmail.com>
2020-07-13 11:48:57 +0200

    Delete obsolete line

--- src/main/scala/Messages.scala
+++ src/main/scala/Messages.scala
@@ -5,7 +5,6 @@ case object AskValue
 case class GiveValue[T <: Node: ClassTag](value: Double)
 case class AutoValue[T <: Node: ClassTag](value: Double)
 case class MakeGrid(n: Int)
-//case class MakeNetwork[T <: Node: ClassTag](networkType: ClassTag[T], networkShape: String, params: Map[String, Any])
 case class MakeNetwork[T <: Node](networkShape: String, params: Map[String, Any])(implicit val ct: ClassTag[T])
 case class Broadcast(message: Any)
 case class CommAction(command: String)
