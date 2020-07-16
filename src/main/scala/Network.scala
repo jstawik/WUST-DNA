7bd71e0d10c11856b2d0f61d9966d197cb00d7e3
jstawik <jakub.stawik@gmail.com>
2020-07-17 00:59:46 +0200

    Implement grid clique graph, fix makeLine bug

--- src/main/scala/Network.scala
+++ src/main/scala/Network.scala
@@ -22,14 +22,50 @@ class Network extends Actor{
     case AskValue => sender() ! value()
     case MakeGrid(n) => makeGrid[PropagateMax](n)
     case f @ MakeNetwork(networkShape, params) => networkShape match {
-      case "grid" => makeGrid((params getOrElse ("n", 100)).asInstanceOf[Int])(f.ct)
+      case "grid" => makeGrid((params getOrElse ("side", 100)).asInstanceOf[Int])(f.ct)
       case "line" => makeLine((params getOrElse ("n", 100)).asInstanceOf[Int])(f.ct)
+      case "gridClique" => makeGridClique((params getOrElse ("side", 100)).asInstanceOf[Int], (params getOrElse("csize", 10)).asInstanceOf[Int])(f.ct)
       case _ => logger.error(s"Unhandled message from ${sender().path.name}" + s" unknown networkShape: $networkShape")
     }
     case CommAction("maxPropagation") => nodes.values.map(n => n ! CommAction("broadcastValue"))
     case CommAction("plotGrid") => Plotter.makeHeatMap(gridView(), "Grid View")
     case SetValue(node, value) => nodes(node) ! GiveValue(value)
     case _ => logger.error(s"Unhandled message from ${sender().path.name}")
+  }
+
+  /**
+   * Adds a Grid-Clique graph of nodes. First 4 nodes in clique are the gateways, mapping being 0123 <-> NWES
+   * @param side Side of the macro grid
+   * @param csize Count of nodes in a clique
+   * @tparam T Type of node
+   */
+  def makeGridClique[T <: Node: ClassTag](side: Int, csize: Int): Unit ={
+    val coord = (x: Int, y:Int, i:Int) => s"node_${x}_${y}_$i"
+    for(x <- 0 until side){
+      for(y <- 0 until side){
+        for(c <- 0 until csize){
+          val newNode = context.actorOf(Props(classTag[T].runtimeClass, self), coord(x,y,c))
+          nodes = nodes + (coord(x,y,c) -> newNode)
+        }
+      }
+    }
+    for(x <- 0 until side){
+      for(y <- 0 until side){
+        for(c <- 0 until csize){
+          val newNeighs = (i: Int) => for{j <- 0 until csize if j != i} yield j
+          for(i <- newNeighs(c)) nodes(coord(x, y, c)) ! GiveNeighbour(nodes(coord(x,y,i)))
+        }
+        if(x-1 >= 0) nodes(coord(x,y,0)) ! GiveNeighbour(nodes(coord(x-1,y,3)))
+        if(x+1 < side) nodes(coord(x,y,3)) ! GiveNeighbour(nodes(coord(x+1,y,0)))
+        if(y-1 >= 0) nodes(coord(x,y,1)) ! GiveNeighbour(nodes(coord(x,y-1,2)))
+        if(y-1 < side) nodes(coord(x,y,2)) ! GiveNeighbour(nodes(coord(x,y+1,1)))
+
+      }
+    }
+    logger.debug("makeGridClique ran")
+    logger.debug(s"nodes is now: $nodes")
+    scrambleValues[T]()
+    nodes.foreach(_._2 ! CommAction("networkReady"))
   }
 
   def makeGrid[T <: Node: ClassTag](side: Int): Unit = {
@@ -56,6 +92,7 @@ class Network extends Actor{
     val coord = (i: Int) => s"node_$i"
     for(i <- 0 until n){
       val newNode = context.actorOf(Props(classTag[T].runtimeClass, self), coord(i))
+      nodes = nodes + (coord(i) -> newNode)
     }
     for(i <- 0 until n){
       val newNeighs = (i: Int) => List(i-1, i+1).filter(_>=0).filter(_<n)
--- src/main/scala/Simulator.scala
+++ src/main/scala/Simulator.scala
@@ -1,23 +1,23 @@
-import akka.actor.{ActorSystem, Props}
+import akka.actor.{ActorRef, ActorSystem, Props}
 
 import scala.reflect.ClassTag
 
 object Simulator {
   def main(args: Array[String]): Unit = {
-    maxPropDemo()
-  }
-
-  def maxPropDemo(): Unit = {
     val system = ActorSystem("simulation")
     val network = system.actorOf(Props[Network], "network")
+    maxPropDemo(network)
+    system.terminate()
+  }
+
+  def maxPropDemo(network: ActorRef): Unit = {
     def gridUpdate(): Unit = {
       network ! CommAction("maxPropagation")
       network ! CommAction("plotGrid")
-      Thread.sleep(500)
+      Thread.sleep(1000)
     }
-    network ! MakeNetwork[PropagateMax]("grid", Map("n" -> 500))
-    for(i <- 1 to 10) gridUpdate()
-    system.terminate()
+    network ! MakeNetwork[PropagateMax]("grid", Map("side" -> 20))
+    for(i <- 1 to 20) gridUpdate()
   }
 }
 
